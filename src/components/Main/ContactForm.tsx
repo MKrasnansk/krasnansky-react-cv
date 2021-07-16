67eb1a2f1791a088e41543475cdbeeba8bfa024f
MKrasnansk <miso.krasnansky@gmail.com>
2021-07-16 14:28:05 +0200

    a

--- src/components/Main/ContactForm.tsx
+++ src/components/Main/ContactForm.tsx
@@ -114,7 +114,7 @@ export const ContactForm: React.FC = () => {
       };
       try {
         await axios
-          .post("https://api-cv-mail.herokuapp.com/api/contact", objectMessage)
+          .post(`https://api-cv-mail.herokuapp.com:${process.env.PORT || 55058}/api/contact`, objectMessage)
           .then((response: AxiosResponse) => {
             if (response.status === 201) {
               setSend(true);
