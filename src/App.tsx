06775f33fae739154d59f1b05eb5ac4668aa8029
MKrasnansk <miso.krasnansky@gmail.com>
2021-07-17 21:37:55 +0200

    bug import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { Box, CssBaseline } from "@material-ui/core";
+import { Box, CssBaseline, ThemeProvider } from "@material-ui/core";
 import * as React from "react";
-import { ThemeProvider } from "styled-components";
 import "./App.scss";
 import { MainLayout } from "./components/MainLayout";
 import { theme } from "./theme";
