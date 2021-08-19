b6811fde193ddd9d72ca2500c22a0846fe95db76
MKrasnansk <miso.krasnansky@gmail.com>
2021-08-19 15:47:50 +0200

    sleep prevent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import React, { useEffect } from "react";
 import { Box, CssBaseline, ThemeProvider } from "@material-ui/core";
-import * as React from "react";
 import "./App.scss";
 import { MainLayout } from "./components/MainLayout";
 import { theme } from "./theme";
+import axios, { AxiosResponse, AxiosTransformer } from "axios";
 
 export const App: React.FC = () => {
+  useEffect(() => {
+    axios
+      .get("https://nodemail-email.herokuapp.com/")
+      .then(async (res: AxiosResponse<AxiosTransformer>) => {
+        console.log(await res.status);
+        await formLogs(res.status)
+        
+      });
+  }, []);
+  const formLogs = (event:number)=>{
+    if (event === 200) {
+      console.log('form is ready');
+    }else{
+      console.log('backend is asleep');
+    }
+  }
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
